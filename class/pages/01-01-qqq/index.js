b2b4e9a8d94ee9d6f7d215daee2f0bb1cfa4273b
Gusto <lhg0616@gmail.com>
2023-01-04 23:33:20 +0900

    class commit

---
+++ class/pages/01-01-qqq/index.js
@@ -0,0 +1,10 @@
+export default function Qqq() {
+  return (
+    <div>
+      <div>
+        <p>철수</p>
+        <div>aaabbbccc</div>
+      </div>
+    </div>
+  );
+}
---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,16 @@
+import { Email, EmailInput } from '../../styles/emotion';
+
+export default function EmotionPage() {
+  // js
+
+  return (
+    // jsx (html)
+    <div>
+      <Email>이메일</Email>
+      <EmailInput type="text" name="" id="" />
+      <button>클릭하세요!</button>
+      <img src="/vercel.svg" alt="" />
+      <p>didididi</p>
+    </div>
+  );
+}
