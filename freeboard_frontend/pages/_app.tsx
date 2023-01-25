85700cae6b74f2d756eec6840b1d29f0d6cf3655
Gusto <lhg0616@gmail.com>
2023-01-25 22:54:35 +0900

    추가 eslint, prettier

--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -1,11 +1,11 @@
-import { AppProps } from 'next/app';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { AppProps } from "next/app";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-import '../styles/globals.css';
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
-    uri: 'https://backendonline.codebootcamp.co.kr/graphql',
+    uri: "https://backendonline.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(),
   });
 
